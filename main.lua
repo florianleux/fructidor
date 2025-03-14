c02addefa8fb112461f5de5cbefd785e8ec704de
Florian <florian.leux@backmarket.com>
2025-03-14 17:17:15 +0100

    Add dice rolling

--- main.lua
+++ main.lua
@@ -1,25 +1,8 @@
--- Point d'entrée principal Fructidor
-local GameState = require('src.states.game_state')
-
 function love.load()
-    -- Initialisation du jeu
-    gameState = GameState.new()
-end
-
-function love.update(dt)
-    -- Mise à jour logique du jeu
-    gameState:update(dt)
+    love.graphics.setBackgroundColor(1, 1, 1)
 end
 
 function love.draw()
-    -- Rendu du jeu
-    gameState:draw()
-end
-
-function love.mousepressed(x, y, button)
-    gameState:mousepressed(x, y, button)
-end
-
-function love.mousereleased(x, y, button)
-    gameState:mousereleased(x, y, button)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print("COUCOU", 300, 200, 0, 4, 4)
 end
