63219b8ec76142304db5622601a9f6d2e7f3470e
Florian Leux <10267793+florianleux@users.noreply.github.com>
2025-03-17 10:19:05 +0100

    Séparation de la logique métier et du rendu dans Garden

--- src/entities/garden.lua
+++ src/entities/garden.lua
@@ -1,5 +1,4 @@
 -- Entité Jardin (grille de jeu)
-local Plant = require('src.entities.plant')
 local Constants = require('src.utils.constants')
 
 local Garden = {}
@@ -26,32 +25,6 @@ function Garden.new(width, height)
     return self
 end
 
-function Garden:draw()
-    local cellWidth = 70
-    local cellHeight = 70
-    local offsetX = 50
-    local offsetY = 180  -- Modifié de 50 à 180 pour aligner avec le système de drag & drop
-    
-    -- Dessiner les cellules
-    for y = 1, self.height do
-        for x = 1, self.width do
-            local posX = offsetX + (x-1) * cellWidth
-            local posY = offsetY + (y-1) * cellHeight
-            
-            -- Dessiner la case
-            love.graphics.setColor(0.8, 0.7, 0.5)
-            love.graphics.rectangle("fill", posX, posY, cellWidth, cellHeight)
-            love.graphics.setColor(0.4, 0.4, 0.4)
-            love.graphics.rectangle("line", posX, posY, cellWidth, cellHeight)
-            
-            -- Dessiner plante si présente
-            if self.grid[y][x].plant then
-                self.grid[y][x].plant:draw(posX, posY, cellWidth, cellHeight)
-            end
-        end
-    end
-end
-
 function Garden:placePlant(plant, x, y)
     if x > 0 and x <= self.width and y > 0 and y <= self.height then
         if not self.grid[y][x].plant then
@@ -63,6 +36,40 @@ function Garden:placePlant(plant, x, y)
         end
     end
     return false
+end
+
+-- Obtenir une cellule du jardin
+function Garden:getCell(x, y)
+    if x > 0 and x <= self.width and y > 0 and y <= self.height then
+        return self.grid[y][x]
+    end
+    return nil
+end
+
+-- Récupérer les cellules adjacentes à une position
+function Garden:getAdjacentCells(x, y)
+    local adjacent = {}
+    
+    -- Vérifier les 4 directions (haut, droite, bas, gauche)
+    local directions = {
+        {x=0, y=-1}, -- haut
+        {x=1, y=0},  -- droite
+        {x=0, y=1},  -- bas
+        {x=-1, y=0}  -- gauche
+    }
+    
+    for _, dir in ipairs(directions) do
+        local newX, newY = x + dir.x, y + dir.y
+        if newX > 0 and newX <= self.width and newY > 0 and newY <= self.height then
+            table.insert(adjacent, {
+                x = newX,
+                y = newY,
+                cell = self.grid[newY][newX]
+            })
+        end
+    end
+    
+    return adjacent
 end
 
 return Garden
