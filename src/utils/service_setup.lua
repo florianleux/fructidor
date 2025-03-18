43dcb61d8dd82f174ac39faedd944d3d91e400ad
Florian Leux <10267793+florianleux@users.noreply.github.com>
2025-03-19 00:04:32 +0100

    Marquer service_setup comme déprécié

--- src/utils/service_setup.lua
+++ src/utils/service_setup.lua
@@ -1,38 +1,16 @@
 -- Configuration du système de services pour Fructidor
+-- DÉPRÉCIÉ: Ce module est maintenu uniquement pour compatibilité
+-- et sera supprimé dans une version future.
+-- Utilisez l'injection de dépendances directe via le constructeur à la place.
+
 local Services = require('src.utils.services')
-local PlantRenderer = require('src.ui.plant_renderer')
-local GardenRenderer = require('src.ui.garden_renderer')
-local CardRenderer = require('src.ui.card_renderer')
 
 -- Module pour initialiser tous les services au démarrage de l'application
 local ServiceSetup = {}
 
--- Enregistrer les factories pour les renderers
-function ServiceSetup.registerFactories()
-    -- Enregistrer les renderers en tant que singletons via des factories
-    Services.registerFactory("PlantRenderer", function()
-        return PlantRenderer.new()
-    end)
-    
-    Services.registerFactory("GardenRenderer", function()
-        return GardenRenderer.new()
-    end)
-    
-    Services.registerFactory("CardRenderer", function()
-        return CardRenderer.new()
-    end)
-    
-    -- Autres factories peuvent être ajoutées ici à l'avenir
-    
-    return true
-end
-
 -- Fonction d'initialisation à appeler une seule fois au démarrage
 function ServiceSetup.initialize(systems)
     systems = systems or {}
-    
-    -- Enregistrer les factories
-    ServiceSetup.registerFactories()
     
     -- Enregistrer les instances des systèmes principaux
     local instances = {}
@@ -43,6 +21,7 @@ function ServiceSetup.initialize(systems)
     if systems.gameState then instances.GameState = systems.gameState end
     if systems.dragDrop then instances.DragDrop = systems.dragDrop end
     if systems.uiManager then instances.UIManager = systems.uiManager end
+    if systems.gardenRenderer then instances.GardenRenderer = systems.gardenRenderer end
     
     -- Ajouter le ScaleManager s'il est fourni et initialisé
     if systems.scaleManager then
