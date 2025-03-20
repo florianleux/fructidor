45f76f15667e80d08e5a19a781d52d171a2eb2d5
Florian <florian.leux@backmarket.com>
2025-03-20 15:23:40 +0100

    base code

---
+++ conf.lua
@@ -0,0 +1,15 @@
+function love.conf(t)
+    -- Save file
+    t.identity = 'data/saves'
+
+    -- Window settings
+    t.window.width = 1920 -- The window width (number)
+    t.window.height = 1080 -- The window height (number)
+    t.window.fullscreen = true
+    t.window.fullscreentype = "desktop"
+    t.window.resizable = true
+
+    t.window.title = "Fructidor"
+
+
+end
---
+++ main.lua
@@ -0,0 +1,17 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    local color = require("utils/convertColor")
+    windowWidth, windowHeight = love.window.getDimensions()
+    xCenter, yCenter = windowWidth / 2, windowHeight / 2
+
+    love.graphics.setColor(color.hex("#FF0000"))
+    love.graphics.rectangle("fill", t.window.width / 2 - 100, t.window.height / 2 - 100, 1000, 200)
+end
+
