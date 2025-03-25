0a9b1f9ddee661116754703f530e719e535fcaeb
Florian <florian.leux@backmarket.com>
2025-03-25 12:12:44 +0100

    drag drop

--- conf.lua
+++ conf.lua
@@ -1,15 +1,14 @@
 function love.conf(t)
     -- Save file
     t.identity = 'data/saves'
+    t.console = true
 
     -- Window settings
-    t.window.width = 1920 -- The window width (number)
+    t.window.width = 1920  -- The window width (number)
     t.window.height = 1080 -- The window height (number)
     t.window.fullscreen = true
     t.window.fullscreentype = "desktop"
     t.window.resizable = true
 
     t.window.title = "Fructidor"
-
-
 end
